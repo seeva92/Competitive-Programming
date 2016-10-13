91695d917877eaaf209d6d40e3b7486fbba6dc57
Sivakumar K R <seevaathome1@gmail.com>
2016-10-13 18:18:01 +0530

    SimplifiedChessdoneg

--- HackerRank/WeekOfCode24/SimplifiedChessEngine.cpp
+++ HackerRank/WeekOfCode24/SimplifiedChessEngine.cpp
@@ -2,12 +2,6 @@
 const int mod = 1e9 + 7;
 using namespace std;
 int g, w, b, m;
-inline bool check(vector<string> &board, char c) {
-	for (auto i : board)
-		for (auto j : i)
-			if (j == c) return false;
-	return true;
-}
 inline bool isvalid(vector<string> &board, char c, int i, int j) {
 	if (('A' <= c && c <= 'Z') && ('a' <= board[i][j] && board[i][j] <= 'z')) return true;
 	if (('a' <= c && c <= 'z') && ('A' <= board[i][j] && board[i][j] <= 'Z')) return true;
@@ -78,76 +72,45 @@ inline vector<pair<int, int>> possible_moves(vector<string> & board, char c, int
 	}
 	return moves;
 }
-inline bool compute(vector<string> board, map<char, pair<int, int>> wht, map<char, pair<int, int>> blk, int m, bool type) {
-	if (!type && check(board, 'q')) return true;
-	else if (type && check(board, 'Q')) return false;
+inline bool compute(vector<string> board,  int m, bool type, bool kill) {
+	if (!type && kill) return true;
+	else if (type && kill) return false;
 	if (m == 0)return false;
-
 	bool res = !type;
 	if (type) {
-		for (auto &i : wht) {
-			if (i.second.first == -1 && i.second.second == -1) continue;
-			auto moves = possible_moves(board, board[i.second.first][i.second.second], i.second.first, i.second.second);
-			for (auto k : moves) {
-				char temp = board[k.first][k.second];
-				//change blk
-				pair<int, int> blkpos;
-				if ('a' <= temp && temp <= 'z')
-				{
-					blkpos = blk[temp];
-					blk[temp] = { -1, -1};
-				}
-				board[k.first][k.second] = board[i.second.first][i.second.second];
-				board[i.second.first][i.second.second] = '*';
+		for (int i = 0; i < int(board.size()); i++) {
+			for (int j = 0; j < int(board[i].size()); j++) {
+				if (board[i][j] == '*' || ('a' <= board[i][j] && board[i][j] <= 'z')) continue;
+				auto moves = possible_moves(board, board[i][j], i, j);
+				for (auto k : moves) {
+					char temp = board[k.first][k.second];
+					board[k.first][k.second] = board[i][j];
+					board[i][j] = '*';
+					if (!res) res = res || compute(board, m - 1, !type, temp == 'q');
+					board[i][j] = board[k.first][k.second];
+					board[k.first][k.second] = temp;
 
-				//change wht
-				auto pos = i.second;
-				i.second.first = k.first; i.second.second = k.second;
-				if (!res) res = res || compute(board, wht, blk, m - 1, !type);;
-				//update wht
-				i.second = pos;
-				board[i.second.first][i.second.second] = board[k.first][k.second];
-				board[k.first][k.second] = temp;
-				//update blk
-				if ('a' <= temp && temp <= 'z') blk[temp] = blkpos;
-				if (res) break;
+					if (res) return res;
+				}
 			}
-			if (res) break;
 		}
 	} else {
-		for (auto &i : blk) {
-			if (i.second.first == -1 && i.second.second == -1) continue;
-
-			auto moves = possible_moves(board, board[i.second.first][i.second.second], i.second.first, i.second.second);
-			for (auto k : moves) {
-				char temp = board[k.first][k.second];
-
-				//change wht
-				pair<int, int> whtpos;
-				if ('A' <= temp && temp <= 'Z')
-				{
-					whtpos = wht[temp];
-					wht[temp] = { -1, -1};
+		for (int i = 0; i < int(board.size()); i++) {
+			for (int j = 0; j < int(board[i].size()); j++) {
+				if (board[i][j] == '*' || ('A' <= board[i][j] && board[i][j] <= 'Z')) continue;
+				auto moves = possible_moves(board, board[i][j], i, j);
+				for (auto k : moves) {
+					char temp = board[k.first][k.second];
+					board[k.first][k.second] = board[i][j];
+					board[i][j] = '*';
+					if (res) res = res && compute(board, m - 1, !type, temp == 'Q');
+					board[i][j] = board[k.first][k.second];
+					board[k.first][k.second] = temp;
+					if (!res) return res;
 				}
-				board[k.first][k.second] = board[i.second.first][i.second.second];
-				board[i.second.first][i.second.second] = '*';
-				//change blk
-				auto pos = i.second;
-				i.second.first = k.first; i.second.second = k.second;
-				if (res) res = res && compute(board, wht, blk, m - 1, !type);;
-				//update blk
-				i.second = pos;
-				board[i.second.first][i.second.second] = board[k.first][k.second];
-				board[k.first][k.second] = temp;
-
-				//update blk
-				if ('A' <= temp && temp <= 'Z') wht[temp] = whtpos;
-				if (!res) break;
 			}
-			if (!res) break;
 		}
 	}
-
 	return res;
 }
 int main() {
@@ -162,23 +125,18 @@ int main() {
 		cin >> w >> b >> m;
 		string s = "****";
 		vector<string> board;
-		map<char, pair<int, int>> blk, wht;
+
 		for (int i = 1; i <= 4; i++) board.push_back(s);
 		char p, c; int r;
 		for (int i = 1; i <= w; i++) {
 			cin >> p >> c >> r; //cout << p << " " << c << " " << r << '\n';
 			board[4 - r][c - 'A'] = toupper(p);
-			wht[toupper(p)] = {4 - r, c - 'A'};
 		}
 		for (int i = 1; i <= b; i++) {
 			cin >> p >> c >> r; //cout << p << " " << c << " " << r << '\n';
 			board[4 - r][c - 'A'] = tolower(p);
-			blk[tolower(p)] = {4 - r, c - 'A'};
 		}
-		// for (auto i : board)
-		// cout << i << '\n';
-		// cout << '\n';
-		if (compute(board, wht, blk, m, true)) cout << "YES\n";
+		if (compute(board, m, true, false)) cout << "YES\n";
 		else cout << "NO\n";
 
 	}
